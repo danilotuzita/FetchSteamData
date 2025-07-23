d7633bd5d12f5791774be6070805e5d42e62e9c0
Danilo Sanchez Tuzita <danilo_st@hotmail.com>
2025-07-24 00:36:19 +0900

    #14 - Refactor update_owned_games

--- app/service/steam/steam_game_service.py
+++ app/service/steam/steam_game_service.py
@@ -9,29 +9,26 @@ from app.service.game_service import GameService
 class SteamGameService:
     @staticmethod
     def update_owned_games() -> list[GetOwnedGamesResponseGame]:
+        return SteamGameService.check_owned_games() + SteamGameService.check_recently_played_games()
+
+    @staticmethod
+    def check_owned_games() -> list[GetOwnedGamesResponseGame]:
         owned_games_response = SteamServiceApi.get_owned_games()
-        if not owned_games_response:
+        if owned_games_response is None:
             logging.error("Failed to get owned games from Steam API.")
             return []
-        SteamGameService.check_owned_games(owned_games_response.games)
-        recently_played_games = SteamServiceApi.get_recently_played_games()
-        if not recently_played_games:
-            logging.error("Failed to get recently played games from Steam API.")
-            return []
-        SteamGameService.check_recently_played_games(recently_played_games.games)
-        return owned_games_response.games + [game.to_owned_game_response_game() for game in recently_played_games.games]
-
-    @staticmethod
-    def check_owned_games(games: list[GetOwnedGamesResponseGame]):
-        logging.info(f'Checking Owned Games... Total={len(games)}')
-        for game_api in games:
+        logging.info(f'Checking Owned Games... Total={len(owned_games_response.games)}')
+        for game_api in owned_games_response.games:
             GameService.put_game(game_api.appid, game_api.name)
+        return owned_games_response.games
 
     @staticmethod
-    def check_recently_played_games(recently_played_games: list[GetRecentlyPlayedGamesResponseGame]):
-        if not recently_played_games:
+    def check_recently_played_games() -> list[GetOwnedGamesResponseGame]:
+        recently_played_games_response = SteamServiceApi.get_recently_played_games()
+        if recently_played_games_response is None:
             logging.error("Failed to get recently played games from Steam API.")
             return []
-        logging.info(f'Checking Recently Played Games... Total={len(recently_played_games)}')
-        for game in recently_played_games:
+        logging.info(f'Checking Recently Played Games... Total={len(recently_played_games_response.games)}')
+        for game in recently_played_games_response.games:
             GameService.put_shared_game(game.appid, game.name)
+        return [game.to_owned_game_response_game() for game in recently_played_games_response.games]
